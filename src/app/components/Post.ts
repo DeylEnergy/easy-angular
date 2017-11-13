11e6c715a670c9ab72c41facb7922f613a35e2f0
Deyl <soulvisman@gmail.com>
2017-11-13 22:49:54 +0600

    Revising Components

---
+++ src/app/components/Post.ts
@@ -0,0 +1,6 @@
+export interface Article{
+  title:string,
+  text:string,
+  author:string,
+  rating:number
+}
--- src/app/components/sample.component.ts
+++ src/app/components/sample.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './Post'
 
 @Component ({
   selector: 'sample',
@@ -8,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class SampleComponent {
   action:string = 'working';
+  post:Article;
+  constructor(){
+    this.post = {
+      title: 'First Post',
+      text: 'This is a short desctiption',
+      author: 'Deyl',
+      rating: 5
+    };
+    console.log(this.post);
+    this.post = {
+      title: 'Second Post',
+      text: 'This is a short desctiption',
+      author: 'Deyl2',
+      rating: 7
+    };
+    console.log(this.post);
+  }
 }
