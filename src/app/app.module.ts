4730c4d8a21c6825b8d6f3fb1ee4c524338de6e1
Deyl <soulvisman@gmail.com>
2017-11-16 14:57:28 +0600

    ngModel & 2 Way Data Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SampleComponent } from './components/sample/sample.component';
@@ -12,7 +13,8 @@ import { EventsFormsComponent } from './components/events-forms/events-forms.com
     EventsFormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
