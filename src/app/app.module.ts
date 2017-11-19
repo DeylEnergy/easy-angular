c3b5f43d3d8cd3110ca697b51a2fdea534f17580
Deyl <soulvisman@gmail.com>
2017-11-19 12:42:31 +0600

    Router added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 import { SampleComponent } from './components/sample/sample.component';
 import { EventsFormsComponent } from './components/events-forms/events-forms.component';
 import { DataComponent } from './components/data/data.component';
@@ -11,9 +13,17 @@ import { DataComponent } from './components/data/data.component';
 import { UserService } from './services/users/users.service';
 import { FakeUsersService } from './services/fake-users/fake-users.service';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'examples', component: SampleComponent},
+  {path: 'events', component: EventsFormsComponent},
+  {path: 'users', component: DataComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     SampleComponent,
     EventsFormsComponent,
     DataComponent
@@ -21,7 +31,8 @@ import { FakeUsersService } from './services/fake-users/fake-users.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     UserService,
